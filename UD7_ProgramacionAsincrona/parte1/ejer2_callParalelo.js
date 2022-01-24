4a1936c04ee9a5210f166d045494a112b0b194c4
Jaime Perez Castellanos <jaipecas89@gmail.com>
2022-01-24 18:28:17 +0100

    cambios ejer 2

--- UD7_ProgramacionAsincrona/parte1/ejer2_callParalelo.js
+++ UD7_ProgramacionAsincrona/parte1/ejer2_callParalelo.js
@@ -1,20 +1,24 @@
 const asyncRequest = require('../parte1/async_request');
 
 function print(resourcesFound) {
-    if (Object.keys(resourcesFound).includes('resource1')) {
+    if (includeResource(resourcesFound, 'resource1')) {
         printResource(resourcesFound['resource1']);
         resourcesFound['resource1'] = undefined;
 
-        if (Object.keys(resourcesFound).includes('resource2')) {
+        if (includeResource(resourcesFound, 'resource2')) {
             printResource(resourcesFound['resource2']);
             resourcesFound['resource2'] = undefined;
 
-            if (Object.keys(resourcesFound).includes('resource3')) {
+            if (includeResource(resourcesFound, 'resource3')) {
                 printResource(resourcesFound['resource3']);
                 console.log('¡Completado!');
             }
         }
     }
+}
+
+function includeResource(resourcesFound, resourceSearch) {
+    return Object.keys(resourcesFound).includes(resourceSearch);
 }
 
 function printResource(data) {
